c6582eaa5d399091f911a7b306f14d0111267123
Jamshidbke <jama25jama25@mail.ru>
2025-12-12 16:52:55 +0500

    Adjust Cart page layout and integrate updated components

--- cart.jsx
+++ cart.jsx
@@ -1,7 +1,14 @@
 import { createRoot } from "react-dom/client";
+import { CartApp } from "./src/components/CartApp";
 import { CartBadge } from "./src/components/CartBadge";
 
+const cartContainer = document.getElementById("cart__container");
 const cartRoot = document.getElementById("cart__root");
+
+if (cartContainer) {
+  createRoot(cartContainer).render(<CartApp />);
+}
+
 if (cartRoot) {
   createRoot(cartRoot).render(<CartBadge />);
 }
