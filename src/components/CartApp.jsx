ea3ff568370c4fc8f40715d08ec684d6d955036d
Jamshidbke <jama25jama25@mail.ru>
2025-12-12 16:52:41 +0500

    Update CartApp component structure and behavior

--- src/components/CartApp.jsx
+++ src/components/CartApp.jsx
@@ -11,10 +11,6 @@ export const CartApp = () => {
   }, []);
 
   const totalItems = cart.reduce((sum, item) => sum + item.quantity, 0);
-  const totalPrice = cart.reduce(
-    (sum, item) => sum + item.price * item.quantity,
-    0
-  );
 
   return (
     <div className="cart__container">
